6428dcab438ec9b7d76c5380693986a73ddc4ec7
Valery Kovalev <robinsout@gmail.com>
2019-11-06 23:54:44 +0300

    help output

--- src/index.js
+++
@@ -1,8 +0,0 @@
-const program = require('commander');
-
-program
-  .name('gendiff')
-  .version('0.0.1')
-  .usage('gendiff [options] <firstConfig> <secondConfig>');
-
-program.parse(process.argv);
