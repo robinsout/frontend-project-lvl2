5e9db061fe207f85b6237a5a9f2bc93ee0e7230d
Valery Kovalev <robinsout@gmail.com>
2019-11-14 21:04:54 +0300

    first test passed

--- src/gendiff.js
+++ src/gendiff.js
@@ -3,5 +3,7 @@ const fs = require('fs');
 export default (firstConfig, secondConfig) => {
   const configBefore = JSON.parse(fs.readFileSync(firstConfig));
   const configAfter = JSON.parse(fs.readFileSync(secondConfig));
-  return { ...configBefore, ...configAfter };
+  const result = { ...configBefore, ...configAfter };
+  console.log(result);
+  return result;
 };
