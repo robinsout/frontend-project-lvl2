e8b5895489dada8e7b01dca1e966163abb7727f8
Valery Kovalev <robinsout@gmail.com>
2019-11-14 20:00:58 +0300

    index added

--- src/gendiff.js
+++ src/gendiff.js
@@ -1,18 +1,3 @@
-#!/usr/bin/env node
-
-const program = require('commander');
-
 const action = (firstConfig, secondConfig) => (`testTEST ${firstConfig} ${secondConfig}`);
-
-program
-  .description('Compares two configuration files and shows a difference.')
-  .version('0.0.1')
-  .option('-f, --format [type]', 'Output format')
-  .arguments('<firstConfig> <secondConfig>')
-  .action((firstConfig, secondConfig) => {
-    action(firstConfig, secondConfig);
-  });
-
-program.parse(process.argv);
 
 export default action;
