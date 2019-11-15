a4c52047031021879cc2ab0dc5b85126d06906ce
Valery Kovalev <robinsout@gmail.com>
2019-11-15 22:40:02 +0300

    version changed

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const program = require('commander');
 
 program
     .description('Compares two configuration files and shows a difference.')
-    .version('0.0.1')
+    .version('0.0.2')
     .option('-f, --format [type]', 'Output format')
     .arguments('<firstConfig> <secondConfig>')
     .action((firstConfig, secondConfig) => {
