fa9ed8115b75f32bf6d6dfb50429ac0571ef75ec
Valery Kovalev <robinsout@gmail.com>
2019-11-15 13:41:29 +0300

    test description updated

--- src/__tests__/gendiff.test.js
+++ src/__tests__/gendiff.test.js
@@ -3,7 +3,7 @@ import genDiff from '../gendiff';
 const fs = require('fs');
 
 describe('tests', () => {
-    test('should open files, parse json and return contents', () => {
+    test('should compare json and return string result', () => {
         const filePath1 = `${__dirname}/__fixtures__/test1.json`;
         const filePath2 = `${__dirname}/__fixtures__/test2.json`;
         const resultPath = `${__dirname}/__fixtures__/result1.txt`;
