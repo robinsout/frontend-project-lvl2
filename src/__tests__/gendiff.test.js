7cef2c2126eb72a222dda565fa54496cc182604c
Valery Kovalev <robinsout@gmail.com>
2019-11-15 21:40:10 +0300

    yaml test added

--- src/__tests__/gendiff.test.js
+++ src/__tests__/gendiff.test.js
@@ -11,4 +11,12 @@ describe('tests', () => {
         const result = fs.readFileSync(resultPath, 'utf8');
         expect(genDiff(filePath1, filePath2)).toEqual(result);
     });
+    test('should compare yaml and return string result', () => {
+        const filePath1 = `${__dirname}/__fixtures__/test1.yaml`;
+        const filePath2 = `${__dirname}/__fixtures__/test2.yaml`;
+        const resultPath = `${__dirname}/__fixtures__/result1.txt`;
+
+        const result = fs.readFileSync(resultPath, 'utf8');
+        expect(genDiff(filePath1, filePath2)).toEqual(result);
+    });
 });
