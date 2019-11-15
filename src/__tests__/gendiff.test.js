76104f3193dd42b6b4cd7b65a69c8e36b3e2176c
Valery Kovalev <robinsout@gmail.com>
2019-11-15 22:57:53 +0300

    refactoring

--- src/__tests__/gendiff.test.js
+++ src/__tests__/gendiff.test.js
@@ -6,19 +6,15 @@ describe('tests', () => {
     const fixturesPath = `${__dirname}/__fixtures__/`;
 
     test('should compare json and return string result', () => {
-        const filePath1 = `${fixturesPath}test1.json`;
-        const filePath2 = `${fixturesPath}test2.json`;
         const resultPath = `${fixturesPath}result1.txt`;
 
         const result = fs.readFileSync(resultPath, 'utf8');
-        expect(genDiff(filePath1, filePath2)).toEqual(result);
+        expect(genDiff(`${fixturesPath}test1.json`, `${fixturesPath}test2.json`)).toEqual(result);
     });
     test('should compare yaml and return string result', () => {
-        const filePath1 = `${fixturesPath}test1.yaml`;
-        const filePath2 = `${fixturesPath}test2.yaml`;
         const resultPath = `${fixturesPath}result1.txt`;
 
         const result = fs.readFileSync(resultPath, 'utf8');
-        expect(genDiff(filePath1, filePath2)).toEqual(result);
+        expect(genDiff(`${fixturesPath}test1.yaml`, `${fixturesPath}test2.yaml`)).toEqual(result);
     });
 });
