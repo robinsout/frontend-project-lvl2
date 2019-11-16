e0c7767da8c9130896d6fbe2d5477aa68a1f5fa6
Valery Kovalev <robinsout@gmail.com>
2019-11-16 17:46:16 +0300

    object key scheduling

--- src/parsers.js
+++ src/parsers.js
@@ -2,14 +2,13 @@ const fs = require('fs');
 const path = require('path');
 const yaml = require('js-yaml');
 
-export default (inputPath) => {
-    switch (path.extname(inputPath)) {
-    case '.json':
-        return JSON.parse(fs.readFileSync(inputPath, 'utf8'));
-    case '.yml':
-    case '.yaml':
-        return yaml.safeLoad(fs.readFileSync(inputPath, 'utf8'));
-    default:
-        return 'unknown format';
-    }
+const parseJson = (inputPath) => JSON.parse(fs.readFileSync(inputPath, 'utf8'));
+const parseYaml = (inputPath) => yaml.safeLoad(fs.readFileSync(inputPath, 'utf8'));
+
+const configMapping = {
+    json: (inputPath) => parseJson(inputPath),
+    yaml: (inputPath) => parseYaml(inputPath),
+    yml: (inputPath) => parseYaml(inputPath),
 };
+
+export default (inputPath) => (configMapping[path.extname(inputPath).substr(1)](inputPath));
