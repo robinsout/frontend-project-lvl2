fe0e9d3399845c1f19a7cda2b6bfeadc73ca6bac
Valery Kovalev <robinsout@gmail.com>
2019-11-16 19:12:00 +0300

    ini support added

--- src/__tests__/gendiff.test.js
+++ src/__tests__/gendiff.test.js
@@ -10,5 +10,6 @@ describe('tests', () => {
         const result = fs.readFileSync(resultPath, 'utf8');
         expect(genDiff(`${fixturesPath}test1.json`, `${fixturesPath}test2.json`)).toEqual(result);
         expect(genDiff(`${fixturesPath}test1.yaml`, `${fixturesPath}test2.yaml`)).toEqual(result);
+        expect(genDiff(`${fixturesPath}test1.ini`, `${fixturesPath}test2.ini`)).toEqual(result);
     });
 });
--- src/parsers.js
+++ src/parsers.js
@@ -1,14 +1,17 @@
 const fs = require('fs');
 const path = require('path');
 const yaml = require('js-yaml');
+const ini = require('ini');
 
 const parseJson = (inputPath) => JSON.parse(fs.readFileSync(inputPath, 'utf8'));
 const parseYaml = (inputPath) => yaml.safeLoad(fs.readFileSync(inputPath, 'utf8'));
+const parseIni = (inputPath) => ini.parse(fs.readFileSync(inputPath, 'utf8'));
 
 const configMapping = {
     json: (inputPath) => parseJson(inputPath),
     yaml: (inputPath) => parseYaml(inputPath),
     yml: (inputPath) => parseYaml(inputPath),
+    ini: (inputPath) => parseIni(inputPath),
 };
 
 export default (inputPath) => (configMapping[path.extname(inputPath).substr(1)](inputPath));
