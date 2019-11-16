dfc3e9910588841d2bba2fab2420328fcaee46f3
Valery Kovalev <robinsout@gmail.com>
2019-11-16 19:49:49 +0300

    using test.each feature

--- src/__tests__/gendiff.test.js
+++ src/__tests__/gendiff.test.js
@@ -4,12 +4,14 @@ const fs = require('fs');
 
 describe('tests', () => {
     const fixturesPath = `${__dirname}/__fixtures__/`;
+    const resultPath = `${fixturesPath}result1.txt`;
+    const result = fs.readFileSync(resultPath, 'utf8');
 
-    test('should compare configs and return string result', () => {
-        const resultPath = `${fixturesPath}result1.txt`;
-        const result = fs.readFileSync(resultPath, 'utf8');
-        expect(genDiff(`${fixturesPath}test1.json`, `${fixturesPath}test2.json`)).toEqual(result);
-        expect(genDiff(`${fixturesPath}test1.yaml`, `${fixturesPath}test2.yaml`)).toEqual(result);
-        expect(genDiff(`${fixturesPath}test1.ini`, `${fixturesPath}test2.ini`)).toEqual(result);
+    test.each([
+        [`${fixturesPath}test1.json`, `${fixturesPath}test2.json`, result],
+        [`${fixturesPath}test1.yaml`, `${fixturesPath}test2.yaml`, result],
+        [`${fixturesPath}test1.ini`, `${fixturesPath}test2.ini`, result],
+    ])('should compare\n%s\nwith\n%s\nand return string result', (configBefore, configAfter, expectedResult) => {
+        expect(genDiff(configBefore, configAfter)).toEqual(expectedResult);
     });
 });
