5618dcda07b52b72c3683f9963317c96fa9a800f
Valery Kovalev <robinsout@gmail.com>
2019-11-26 00:18:31 +0300

    test ast created and nested ast test added

--- src/__tests__/gendiff.test.js
+++ src/__tests__/gendiff.test.js
@@ -3,9 +3,24 @@ import parseConfig from '../parsers';
 
 const fs = require('fs');
 
-describe('check AST', () => {
+
+describe('check flat AST', () => {
     const fixturesPath = `${__dirname}/__fixtures__/flat/`;
     const astResultPath = `${fixturesPath}result_flat_ast.json`;
+    const result = parseConfig(astResultPath);
+
+    test.each([
+        [`${fixturesPath}test1.json`, `${fixturesPath}test2.json`, result],
+        [`${fixturesPath}test1.yaml`, `${fixturesPath}test2.yaml`, result],
+        [`${fixturesPath}test1.ini`, `${fixturesPath}test2.ini`, result],
+    ])('should compare\n%s\n%s\nand return AST result', (configBefore, configAfter, expectedResult) => {
+        expect(genDiff(configBefore, configAfter)).toEqual(expectedResult);
+    });
+});
+
+describe('check AST', () => {
+    const fixturesPath = `${__dirname}/__fixtures__/nested/`;
+    const astResultPath = `${fixturesPath}result_nested_ast.json`;
     const result = parseConfig(astResultPath);
 
     test.each([
