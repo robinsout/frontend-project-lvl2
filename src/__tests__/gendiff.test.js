e70d7acf354401a18bdf789baf35186a1513b375
Valery Kovalev <robinsout@gmail.com>
2019-11-30 22:35:38 +0300

    tests refactoring

--- src/__tests__/gendiff.test.js
+++ src/__tests__/gendiff.test.js
@@ -5,14 +5,14 @@ const fs = require('fs');
 
 
 describe('check flat AST', () => {
-    const flatFixturesPath = `${__dirname}/__fixtures__/flat/`;
-    const astResultPath = `${flatFixturesPath}result_flat_ast.json`;
-    const flatResultAst = parseConfig(astResultPath);
+    const fixturesPath = `${__dirname}/__fixtures__/flat/`;
+    const astResultPath = `${fixturesPath}result_flat_ast.json`;
+    const result = parseConfig(astResultPath);
 
     test.each([
-        [`${flatFixturesPath}test1.json`, `${flatFixturesPath}test2.json`, flatResultAst],
-        [`${flatFixturesPath}test1.yaml`, `${flatFixturesPath}test2.yaml`, flatResultAst],
-        [`${flatFixturesPath}test1.ini`, `${flatFixturesPath}test2.ini`, flatResultAst],
+        [`${fixturesPath}test1.json`, `${fixturesPath}test2.json`, result],
+        [`${fixturesPath}test1.yaml`, `${fixturesPath}test2.yaml`, result],
+        [`${fixturesPath}test1.ini`, `${fixturesPath}test2.ini`, result],
     ])('should compare\n%s\n%s\nand return flat AST result', (configBefore, configAfter, expectedResult) => {
         expect(
             genDiff.compare(
@@ -27,14 +27,14 @@ describe('check flat AST', () => {
 });
 
 describe('check nested AST', () => {
-    const nestedFixturesPath = `${__dirname}/__fixtures__/nested/`;
-    const astResultPath = `${nestedFixturesPath}result_nested_ast.json`;
-    const nestedResultAst = parseConfig(astResultPath);
+    const fixturesPath = `${__dirname}/__fixtures__/nested/`;
+    const astResultPath = `${fixturesPath}result_nested_ast.json`;
+    const result = parseConfig(astResultPath);
 
     test.each([
-        [`${nestedFixturesPath}test1.json`, `${nestedFixturesPath}test2.json`, nestedResultAst],
-        [`${nestedFixturesPath}test1.yaml`, `${nestedFixturesPath}test2.yaml`, nestedResultAst],
-        [`${nestedFixturesPath}test1.ini`, `${nestedFixturesPath}test2.ini`, nestedResultAst],
+        [`${fixturesPath}test1.json`, `${fixturesPath}test2.json`, result],
+        [`${fixturesPath}test1.yaml`, `${fixturesPath}test2.yaml`, result],
+        [`${fixturesPath}test1.ini`, `${fixturesPath}test2.ini`, result],
     ])('should compare\n%s\n%s\nand return nested AST result', (configBefore, configAfter, expectedResult) => {
         expect(
             genDiff.compare(
@@ -55,8 +55,8 @@ describe('flat configs', () => {
 
     test.each([
         [`${fixturesPath}test1.json`, `${fixturesPath}test2.json`, result],
-        // [`${fixturesPath}test1.yaml`, `${fixturesPath}test2.yaml`, result],
-        // [`${fixturesPath}test1.ini`, `${fixturesPath}test2.ini`, result],
+        [`${fixturesPath}test1.yaml`, `${fixturesPath}test2.yaml`, result],
+        [`${fixturesPath}test1.ini`, `${fixturesPath}test2.ini`, result],
     ])('should compare\n%s\n%s\nand return string result', (configBefore, configAfter, expectedResult) => {
         expect(
             genDiff.render(
@@ -76,8 +76,8 @@ describe('nested configs', () => {
 
     test.each([
         [`${fixturesPath}test1.json`, `${fixturesPath}test2.json`, result],
-        //        [`${fixturesPath}test1.yaml`, `${fixturesPath}test2.yaml`, result],
-        //        [`${fixturesPath}test1.ini`, `${fixturesPath}test2.ini`, result],
+        [`${fixturesPath}test1.yaml`, `${fixturesPath}test2.yaml`, result],
+        [`${fixturesPath}test1.ini`, `${fixturesPath}test2.ini`, result],
     ])('should compare\n%s\n%s\nand return string result', (configBefore, configAfter, expectedResult) => {
         expect(
             genDiff.render(
