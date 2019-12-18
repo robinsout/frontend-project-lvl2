f0742290943aac24870d1b7bb78a27665ef214fa
Valery Kovalev <v.kovalev@reg.ru>
2019-12-18 21:10:47 +0300

    plain formatter added

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,23 +1,38 @@
 const _ = require('lodash');
 
-const render = (comparedAst, result = []) => {
+const render = (comparedAst, result = [], parents = '') => {
     _.map(comparedAst, (obj) => {
-        if (!Object.keys(obj).includes('children')) {
-            Object.getOwnPropertyNames(obj).forEach((key) => {
-                result.push(`${key}: ${obj[key]}`);
-            });
+        if (obj.type === '') {
             return result;
         }
-        const braceOrValue = obj.children.length > 0 ? '{' : `${obj.keyValue}`;
-        const type = obj.type === '' ? ' ' : obj.type;
-        result.push(`${type} ${obj.keyName}: ${braceOrValue}`);
+        const currentPropertyIndex = _.indexOf(comparedAst, obj);
+        const fullPropertyName = `${parents}${obj.keyName}`;
 
-        if (obj.children.length > 0) {
-            return render(obj.children, result);
+        if (currentPropertyIndex + 1 < comparedAst.length
+            && comparedAst[currentPropertyIndex + 1].keyName === obj.keyName) {
+            const oldValue = `${obj.children.length > 0 ? '[complex value]' : `'${obj.keyValue}'`}`;
+            const nextProperty = comparedAst[currentPropertyIndex + 1];
+            const newValue = `${nextProperty.children.length > 0 ? '[complex value]' : `'${nextProperty.keyValue}'`}`;
+            const stringToAdd = `Property '${fullPropertyName}' was updated. From ${oldValue} to ${newValue}`;
+            return result.push(stringToAdd);
+        }
+
+        if (obj.type === '-') {
+            const stringToAdd = `Property '${fullPropertyName}' was removed`;
+            return result.push(stringToAdd);
+        }
+
+        if (obj.type === '+' && comparedAst[currentPropertyIndex - 1].keyName !== obj.keyName) {
+            const stringToAdd = `Property '${fullPropertyName}' was added with value: ${obj.children.length > 0 ? '[complex value]' : `'${obj.keyValue}'`}`;
+            return result.push(stringToAdd);
+        }
+
+        if (obj.children && obj.children.length > 0) {
+            return render(obj.children, result, `${fullPropertyName}.`);
         }
         return result;
     });
-    return result.join('\n');
+    return `${result.join('\n')}\n`;
 };
 
 export default render;
