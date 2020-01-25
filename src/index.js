3fdb272bf40a2c5b06591f756516bf3471e99a09
Valery Kovalev <v.kovalev@reg.ru>
2020-01-25 14:54:16 +0300

    plain asciinema updated

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const program = require('commander');
 
 program
   .description('Compares two configuration files and shows a difference.')
-  .version('0.1.2')
+  .version('0.1.3')
   .option('-f, --format [type]', 'Output format', 'diff')
   .arguments('<firstConfig> <secondConfig>')
   .action((firstConfig, secondConfig, options) => {
