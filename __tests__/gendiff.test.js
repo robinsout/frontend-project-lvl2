5c7e9cb6e04b8e053298debd23bb7954e9440aaf
Valery Kovalev <v.kovalev@reg.ru>
2020-02-04 21:54:05 +0300

    path-factory used to make  paths in tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -3,28 +3,23 @@ import genDiff from '../src';
 
 const fixturesPath = `${__dirname}/__fixtures__/`;
 
+const pathFactory = (fileName) => `${__dirname}/__fixtures__/nested/${fileName}`;
+
 describe('compare configs', () => {
   const nestedStringResult = fs.readFileSync(`${fixturesPath}nested/result.txt`, 'utf8');
   const plainFormatResult = fs.readFileSync(`${fixturesPath}/plain.txt`, 'utf8');
   const jsonFormatResult = fs.readFileSync(`${fixturesPath}/string_json_result.txt`, 'utf8');
 
-  const nestedJsonBeforePath = `${fixturesPath}nested/test1.json`;
-  const nestedJsonAfterPath = `${fixturesPath}nested/test2.json`;
-  const nestedYamlBeforePath = `${fixturesPath}nested/test1.yaml`;
-  const nestedYamlAfterPath = `${fixturesPath}nested/test2.yaml`;
-  const nestedIniBeforePath = `${fixturesPath}nested/test1.ini`;
-  const nestedIniAfterPath = `${fixturesPath}nested/test2.ini`;
-
   test.each([
-    [genDiff, nestedJsonBeforePath, nestedJsonAfterPath, nestedStringResult, 'diff'],
-    [genDiff, nestedYamlBeforePath, nestedYamlAfterPath, nestedStringResult, 'diff'],
-    [genDiff, nestedIniBeforePath, nestedIniAfterPath, nestedStringResult, 'diff'],
-    [genDiff, nestedJsonBeforePath, nestedJsonAfterPath, plainFormatResult, 'plain'],
-    [genDiff, nestedYamlBeforePath, nestedYamlAfterPath, plainFormatResult, 'plain'],
-    [genDiff, nestedIniBeforePath, nestedIniAfterPath, plainFormatResult, 'plain'],
-    [genDiff, nestedJsonBeforePath, nestedJsonAfterPath, jsonFormatResult, 'json'],
-    [genDiff, nestedYamlBeforePath, nestedYamlAfterPath, jsonFormatResult, 'json'],
-    [genDiff, nestedIniBeforePath, nestedIniAfterPath, jsonFormatResult, 'json'],
+    [genDiff, pathFactory('test1.json'), pathFactory('test2.json'), nestedStringResult, 'diff'],
+    [genDiff, pathFactory('test1.yaml'), pathFactory('test2.yaml'), nestedStringResult, 'diff'],
+    [genDiff, pathFactory('test1.ini'), pathFactory('test2.ini'), nestedStringResult, 'diff'],
+    [genDiff, pathFactory('test1.json'), pathFactory('test2.json'), plainFormatResult, 'plain'],
+    [genDiff, pathFactory('test1.yaml'), pathFactory('test2.yaml'), plainFormatResult, 'plain'],
+    [genDiff, pathFactory('test1.ini'), pathFactory('test2.ini'), plainFormatResult, 'plain'],
+    [genDiff, pathFactory('test1.json'), pathFactory('test2.json'), jsonFormatResult, 'json'],
+    [genDiff, pathFactory('test1.yaml'), pathFactory('test2.yaml'), jsonFormatResult, 'json'],
+    [genDiff, pathFactory('test1.ini'), pathFactory('test2.ini'), jsonFormatResult, 'json'],
   ])('\nfunction: %s\n   file1:\n%s\n   file2:\n%s\n\n', (action, configBefore, configAfter, expectedResult, format) => {
     expect(action(configBefore, configAfter, format)).toEqual(expectedResult);
   });
